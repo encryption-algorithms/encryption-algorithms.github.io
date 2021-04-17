11f8627626b4a9d742aaadd6e172f6e7b5478b1f
encryption-algorithms <loctp12310@gmail.com>
2021-04-17 18:22:54 +0700

    Test deploy

---
+++ src/main/index.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+import './main.css'
+
+class Main extends Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount () {
+
+    }
+
+    render () {
+
+        return (
+            <div className="main">
+                <div className="main-init">
+                    Hello World
+                    Test Deploy
+                </div>
+                <div className="main-solve">
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Main
